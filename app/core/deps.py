f834fa5cb4f9f44a88d69ac2dfb9089657bb141f
kipyusha <kirya16grachev@gmail.com>
2026-02-27 14:08:52 +0600

    add current user dependency

--- app/core/deps.py
+++ app/core/deps.py
@@ -1,5 +1,29 @@
+
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException
+from jose import jwt
+from sqlalchemy import select
+from app.core.config import settings
+from app.models.user import User
 from app.core.database import SessionLocal
 
 async def get_db():
     async with SessionLocal() as session:
         yield session
+
+oauth2 = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+async def get_current_user(token: str = Depends(oauth2), db = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        user_id = int(payload("sub"))
+    except:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    
+    res = await db.execute(select(User).where(User.id == user_id))
+    user = res.scalar_one_or_none()
+
+    if not user or not user.is_active:
+        raise HTTPException(status_code=401, detail="Inactive user")
+    
+    return user
